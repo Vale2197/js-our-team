3edf415e226c8b44c700c46d20d42d03a2f1938d
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-22 14:54:45 +0200

    struttura base html

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Descizione
+Il nostro compito é quello di rappresentare, utilizzando la struttura dati piú appropriata, una lista di membri di un team.
+Ciascun membro del team avrá come caratteristica name, role, image
+Per le immagini potete mettere il percorso di un immagine presa da internet, provate con lorem pixum (https://picsum.photos/images)
+Todo:
+Stampare in console tutti gli elementi del team e le loro proprieta.
+Esempio
+nome: Ben Green
+role: CEO
+image: https://picsum.photos/id/1025/300/300
+ */
+
