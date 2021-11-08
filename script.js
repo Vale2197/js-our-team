d84281f37768d6de0bd875e8a840b2deddb70feb
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-08 12:19:02 +0100

    completato our team

--- script.js
+++ script.js
@@ -10,3 +10,52 @@ role: CEO
 image: https://picsum.photos/id/1025/300/300
  */
 
+let teamMembers = [
+    {
+        nome: "Michael",
+        ruolo: "Amministratore",
+        immagine: "https://picsum.photos/id/1025/300/300"
+    },
+    {
+        nome: "Pam",
+        ruolo: "Amministratore",
+        immagine: "https://picsum.photos/id/1025/300/300"
+    },
+    {
+        nome: "Jim",
+        ruolo: "Amministratore",
+        immagine: "https://picsum.photos/id/1025/300/300"
+    },
+    {
+        nome: "Dweight",
+        ruolo: "Amministratore",
+        immagine: "https://picsum.photos/id/1025/300/300"
+    },
+    {
+        nome: "Kelly",
+        ruolo: "Amministratore",
+        immagine: "https://picsum.photos/id/1025/300/300"
+    },
+    {
+        nome: "Oscar",
+        ruolo: "Amministratore",
+        immagine: "https://picsum.photos/id/1025/300/300"
+    },
+    {
+        nome: "Angela",
+        ruolo: "Amministratore",
+        immagine: "https://picsum.photos/id/1025/300/300"
+    },
+]
+
+
+for (let i = 0; i < teamMembers.length; i++) {
+    const element = teamMembers[i];
+
+    console.log(element);
+
+            for (const key in element) {
+                console.log(key, element[key]);
+            };
+    
+};
